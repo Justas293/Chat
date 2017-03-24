22dbcdfb9971bdd7383c729e5562d38b4248cd27
Justas <justasrasl@yahoo.com>
2017-03-24 12:34:23 +0200

    Added some UI

--- Chat/ChatForm.Designer.cs
+++ Chat/ChatForm.Designer.cs
@@ -28,20 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.textBoxChannel = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(98, 57);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(233, 20);
+            this.textBoxAddress.TabIndex = 0;
+            // 
+            // textBoxChannel
+            // 
+            this.textBoxChannel.Location = new System.Drawing.Point(98, 99);
+            this.textBoxChannel.Name = "textBoxChannel";
+            this.textBoxChannel.Size = new System.Drawing.Size(232, 20);
+            this.textBoxChannel.TabIndex = 1;
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(98, 145);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(233, 20);
+            this.textBoxUsername.TabIndex = 2;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.buttonConnect.Location = new System.Drawing.Point(142, 188);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(134, 40);
+            this.buttonConnect.TabIndex = 3;
+            this.buttonConnect.Text = "CONNECT";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Server Address:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Channel name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Username:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label4.Location = new System.Drawing.Point(150, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 28);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Welcome!";
             // 
             // ChatForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(304, 261);
+            this.ClientSize = new System.Drawing.Size(401, 244);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.textBoxChannel);
+            this.Controls.Add(this.textBoxAddress);
             this.Name = "ChatForm";
             this.Text = "IRC Chat client";
+            this.UseWaitCursor = true;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.TextBox textBoxChannel;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
---
+++ Chat/MainChatForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Chat
+{
+    partial class MainChatForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxChat = new System.Windows.Forms.RichTextBox();
+            this.richTextBoMessage = new System.Windows.Forms.RichTextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.listBoxUsers = new System.Windows.Forms.ListBox();
+            this.buttonWhisper = new System.Windows.Forms.Button();
+            this.buttonSettings = new System.Windows.Forms.Button();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBoxChat
+            // 
+            this.richTextBoxChat.Location = new System.Drawing.Point(228, 59);
+            this.richTextBoxChat.Name = "richTextBoxChat";
+            this.richTextBoxChat.ReadOnly = true;
+            this.richTextBoxChat.Size = new System.Drawing.Size(452, 291);
+            this.richTextBoxChat.TabIndex = 0;
+            this.richTextBoxChat.Text = "";
+            // 
+            // richTextBoMessage
+            // 
+            this.richTextBoMessage.Location = new System.Drawing.Point(229, 371);
+            this.richTextBoMessage.Name = "richTextBoMessage";
+            this.richTextBoMessage.Size = new System.Drawing.Size(366, 79);
+            this.richTextBoMessage.TabIndex = 1;
+            this.richTextBoMessage.Text = "";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(601, 371);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(78, 78);
+            this.buttonSend.TabIndex = 2;
+            this.buttonSend.Text = "SEND";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            // 
+            // listBoxUsers
+            // 
+            this.listBoxUsers.FormattingEnabled = true;
+            this.listBoxUsers.Location = new System.Drawing.Point(30, 59);
+            this.listBoxUsers.Name = "listBoxUsers";
+            this.listBoxUsers.Size = new System.Drawing.Size(175, 290);
+            this.listBoxUsers.TabIndex = 3;
+            // 
+            // buttonWhisper
+            // 
+            this.buttonWhisper.Location = new System.Drawing.Point(30, 355);
+            this.buttonWhisper.Name = "buttonWhisper";
+            this.buttonWhisper.Size = new System.Drawing.Size(175, 28);
+            this.buttonWhisper.TabIndex = 4;
+            this.buttonWhisper.Text = "Whisper";
+            this.buttonWhisper.UseVisualStyleBackColor = true;
+            // 
+            // buttonSettings
+            // 
+            this.buttonSettings.Location = new System.Drawing.Point(59, 405);
+            this.buttonSettings.Name = "buttonSettings";
+            this.buttonSettings.Size = new System.Drawing.Size(104, 31);
+            this.buttonSettings.TabIndex = 5;
+            this.buttonSettings.Text = "Settings...";
+            this.buttonSettings.UseVisualStyleBackColor = true;
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Location = new System.Drawing.Point(59, 447);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(103, 28);
+            this.buttonDisconnect.TabIndex = 6;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            // 
+            // MainChatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 490);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.buttonSettings);
+            this.Controls.Add(this.buttonWhisper);
+            this.Controls.Add(this.listBoxUsers);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.richTextBoMessage);
+            this.Controls.Add(this.richTextBoxChat);
+            this.Name = "MainChatForm";
+            this.Text = "IRC Chat client";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxChat;
+        private System.Windows.Forms.RichTextBox richTextBoMessage;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.ListBox listBoxUsers;
+        private System.Windows.Forms.Button buttonWhisper;
+        private System.Windows.Forms.Button buttonSettings;
+        private System.Windows.Forms.Button buttonDisconnect;
+    }
+}
