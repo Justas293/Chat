2802d548701e600e37ff1d481d873f69f091e766
Justas <justasrasl@yahoo.com>
2017-03-24 12:03:29 +0200

    Initial commit

--- Chat/ChatForm.Designer.cs
+++ Chat/ChatForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Chat
 {
-    partial class Form1
+    partial class ChatForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ChatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "ChatForm";
+            this.Text = "IRC Chat client";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
